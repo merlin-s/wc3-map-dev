08a69160b5d16486eedbe6b40d3c487845c5c5ee
Merlin Schindlbeck <me_github@lunarstrain.de>
2021-01-30 02:18:00 +0100

    wip hero selection

--- src/CustomUI/wip.ts
+++ src/CustomUI/wip.ts
@@ -1,94 +1,88 @@
-import { TextFrame } from './TextFrame';
 import { FramePosition } from './FramePosition';
 import { Vector2D } from 'Common/Vector2D';
-import { TextFrameData } from './TextFrameData';
-import { Backdrop } from './Backdrop';
-import { Button } from './Button';
 import { FrameTrigger } from './FrameTrigger';
-import { Colorizer } from 'Common/Colorizer';
-import { SliderData } from './SliderData';
-import { Timer } from 'w3ts';
-import { ButtonMenu } from './ButtonMenu';
-import { BasicButton } from './BasicButton';
-import { BasicTitledSlider } from './BasicTitledSlider';
-import { StatusBar } from './StatusBar';
-import { StatusBarData } from './StatusBarData';
-import { TextureData } from './TextureData';
-import { HPBar } from './HPBar';
-import { MPBar } from './MPBar';
-import { LevelBar } from './LevelBar';
-import { ToolTipFrame } from './ToolTipFrame';
-import { StatusBarSimpleFrame } from './StatusBarSimpleFrame';
 import { IconButton } from './AbilityButton';
 import { Icon } from 'Common/Icon';
 import { AbilityButtonHotbar } from './AbilityButtonHotbar';
 import { FrameHelper } from 'Common/FrameHelper';
 import { Constants } from 'Common/Constants';
 import { Logger } from 'Libs/TreeLib/Logger';
-import { BasicTitledBackdrop } from './BasicTitledBackdrop';
-import { BasicTextFrame } from './BasicTextFrame';
+
+
+import { HeroTypes } from '../unitTypes/index';
+import { Timer } from 'w3ts/index';
 
 
 // need to add promise + error catching
 function LoadToc(path: string): boolean {
-	const loaded = BlzLoadTOCFile(path);
-	if(loaded) {
-		Logger.LogDebug("Loaded: " + path);
-	} else {
-		Logger.LogDebug("Failed to load: " + path);
-	}
-	return loaded;
+    const loaded = BlzLoadTOCFile(path);
+    if (loaded) {
+        Logger.LogDebug("Loaded: " + path);
+    } else {
+        Logger.LogDebug("Failed to load: " + path);
+    }
+    return loaded;
 }
 
 // perhaps a map of all ui elements, instead of these globals
-let mainMenu: ButtonMenu;
-const buttonMenus: Map<String, ButtonMenu> = new Map();
 
 export function ui() {
-	Logger.LogDebug("Setting up Custom UI ... ");
+    Logger.LogDebug("Setting up Custom UI ... ");
 
-	let loaded = LoadToc("CustomUI\\templates.toc");
-	
-    const grandpa = BlzGetOriginFrame(ORIGIN_FRAME_GAME_UI,0);
-    
+    LoadToc("CustomUI\\templates.toc");
+
+
+    const grandpa = BlzGetOriginFrame(ORIGIN_FRAME_GAME_UI, 0);
+
+    const chatButtonHandle = BlzGetFrameByName("UpperButtonBarChatButton", 0);
+
     const abilityHotBar = new AbilityButtonHotbar(
-    "abilityButtonHotBar", 
-    grandpa,
-    0,
-    new Vector2D(0.04 * (Constants.maxSubAbilities) + 0.003, 0.08),
-    new FramePosition(FRAMEPOINT_BOTTOM, grandpa, FRAMEPOINT_BOTTOM, 0, 0.16),
-)
-
-for (let i = 0; i < Constants.maxSubAbilities*2; ++i) {
-    let heroId = FourCC('hpal')
-    let icon = new Icon(
-        BlzGetAbilityIcon(heroId)
+        "buttonHotBar",
+        grandpa,
+        0,
+        new Vector2D(0.04 * 4 + 0.003, 0.016),
+        new FramePosition(FRAMEPOINT_BOTTOM, chatButtonHandle, FRAMEPOINT_BOTTOM, 0, -0.04),
     )
-    const abilityButton = new IconButton(
-        "abilityButton" + i,
-        abilityHotBar.frameHandle, 
-        i,
-        new Vector2D(0.04, 0.04), 
-        new FramePosition(FRAMEPOINT_CENTER, grandpa, FRAMEPOINT_CENTER, i*0.04, 0), 
-        icon,
-        BlzGetAbilityTooltip(heroId, 0),
-        BlzGetAbilityExtendedTooltip(heroId, 0)
-    );
-    
-    const abilityButtonTrigger = new FrameTrigger();
-    abilityButtonTrigger.registerFrameEvent(abilityButton.frameHandle, FRAMEEVENT_CONTROL_CLICK);
-    abilityButtonTrigger.addAction(() => {
-        if (GetTriggerPlayer() == GetLocalPlayer()) {
-            abilityButton.setEnabled(false)
-            FrameHelper.loseFocusFromTriggeringFrame();
-        }
-    });
-    
-    
-    BlzFrameSetText(BlzGetFrameByName("MyAbilityIconBarText", i), "");
 
-    abilityHotBar.addButton(abilityButton);
-}
+    let i = 0;
+    for (let heroId of HeroTypes) {
 
-abilityHotBar.autoAlignButtonPositions();
+        heroId = heroId as number;
+        let icon = new Icon(
+            BlzGetAbilityIcon(heroId)
+        )
+
+        const button = new IconButton(
+            "button" + i,
+            abilityHotBar.frameHandle,
+            i,
+            new Vector2D(0.04, 0.04),
+            new FramePosition(FRAMEPOINT_CENTER, grandpa, FRAMEPOINT_CENTER, i * 0.04, 0),
+            icon,
+            BlzGetAbilityTooltip(heroId, 0),
+            BlzGetAbilityExtendedTooltip(heroId, 0)
+        );
+
+        const buttonTrigger = new FrameTrigger();
+        buttonTrigger.registerFrameEvent(button.frameHandle, FRAMEEVENT_CONTROL_CLICK);
+        buttonTrigger.addAction(() => {
+            if (GetTriggerPlayer() == GetLocalPlayer()) {
+                FrameHelper.loseFocusFromTriggeringFrame();
+            }
+            buttonTrigger.destroy();
+            let t = new Timer().start(0.05, false, () => {
+                button.setEnabled(false);
+                t.destroy();
+            })
+        });
+
+
+        BlzFrameSetText(BlzGetFrameByName("MyAbilityIconBarText", i), "");
+
+        abilityHotBar.addButton(button);
+        i = i + 1;
+    }
+
+
+    abilityHotBar.autoAlignButtonPositions();
 }
--- src/unitTypes/index.ts
+++ src/unitTypes/index.ts
@@ -5,3 +5,32 @@ export enum BaseType {
 export enum SpawnType {
     Footman =  FourCC('hfoo')
 };
+
+export const HeroTypes: number[] = [
+    // Human
+    FourCC('Hamg'),
+    FourCC('Hblm'),
+    FourCC('Hmkg'),
+    FourCC('Hpal'),
+    
+        // Orc
+    FourCC('Obla'),
+    FourCC('Ofar'),
+    FourCC('Oshd'),
+    FourCC('Otch'),
+    
+        // Undead
+    FourCC('Ucrl'),
+    FourCC('Udea'),
+    FourCC('Udre'),
+    FourCC('Ulic'),
+    
+        // NightElf
+    FourCC('Edem'),
+    FourCC('Ekee'),
+    FourCC('Emoo'),
+    FourCC('Ewar'),
+    
+        // Custom
+    FourCC('Eevi'),
+];
